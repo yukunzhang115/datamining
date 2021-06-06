106f645d7446e6d9d62215114f15b98be1dc410e
yukunzhang115 <77864795+yukunzhang115@users.noreply.github.com>
2021-06-06 15:44:14 -0700

    Compeled Pagerank.java
    Finished function:
    1. removeNode
    2. convertMatrix
    3. pangrank
    4. rank
    5. show

--- Pagerank.java
+++ Pagerank.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 import java.math.*;
+import java.security.KeyStore.Entry;
 
 // import jdk.javadoc.internal.doclets.formats.html.resources.standard;
 
@@ -8,8 +9,87 @@ class Pagerank {
 
     static final double converge = 0.000000000001;
 
-    public static void removeNode(List<List<Double>> columns, List<Integer> countColumns) {
+    public static void show(Map<Double, String> treeMap) {
+        int count = 0;
+
+        for(Map.Entry<Double,String> entry : treeMap.entrySet()) {
+            double key = entry.getKey();
+            String value = entry.getValue();
+          
+            count++;
+
+            System.out.println(count + ": " + key + " => " + value);
+            
+            // if(count >= 10) break;
+          }
+    }
+
+    public static Map<Double, String> rank(double[] vector, List<String> nameList) {
+        Map<Double, String> treeMap = new TreeMap<>(Collections.reverseOrder());
+
+        for(int i = 0; i < vector.length; i++) {
+            treeMap.put(vector[i], nameList.remove(0));
+        }
+
+        return treeMap;
+    }
+
+    public static double[] pagerank(List<List<Double>> columns) {
+        double[] vector = new double[columns.size()];
+        Arrays.fill(vector, 1.0 / columns.size());
+
+        double[] newVector;
+
+        int times = 0;
+
+        while(true) {
+            times++;
+
+            System.out.println("---" + times + "---");
+
+            newVector = new double[columns.size()];
+
+            int changeCount = 0;
+
+            for(int j = 0; j < columns.size(); j++) {
+                for(int i = 0; i < columns.size(); i++) {
+                    newVector[j] += columns.get(i).get(j) * vector[i];
+                }
+
+                if(Math.abs(newVector[j] - vector[j]) <= converge) changeCount++;
+
+                // System.out.print(j + " ");
+            }
+            if(changeCount >= vector.length) return newVector;
+
+            // vector = newVector.clone();
+            System.arraycopy(newVector, 0, vector, 0, newVector.length);
+
+            // System.out.println();
+            System.out.println(changeCount);
+            // System.out.println();
+
+        }
+
+
+    }
+
+    public static void convertMatrix(List<List<Double>> columns, List<Integer> countColumns) {
+        for(int i = 0; i < columns.size(); i++) {
+            // System.out.println(i);
+            double num = 1.0 / countColumns.get(i);
+            for(int j = 0; j < columns.size(); j++) {
+                if(columns.get(i).get(j) == 1.0) columns.get(i).set(j, num);
+            }
+        }
+    }
+
+    public static void removeNode(List<List<Double>> columns, List<Integer> countColumns, List<String> nameList) {
+        int count = 0;
+
         while (true) {
+            count++;
+
             boolean isEnd = false;
 
             // find a column with count of 1 or 0
@@ -24,20 +104,12 @@ class Pagerank {
             }
             if(isEnd) break;
 
+            // System.out.println(count + "-columnNumber: " + columnNumber);
+
             // delete that column
             columns.remove(columnNumber);
             countColumns.remove(columnNumber);
-
-            // colsNumber--;
-
-            // System.out.println("---matrix after deleting wrong column---");
-            // for (int i = 0; i < rowsNumber; i++) {
-            //     for (int j = 0; j < colsNumber; j++) {
-
-            //         System.out.print(rows.get(i).get(j) + "\t");
-            //     }
-            //     System.out.println();
-            // }
+            nameList.remove(columnNumber);
 
             // modify count of other columns AND delete that row
             for(int i = 0; i < columns.size(); i++) {
@@ -45,256 +117,13 @@ class Pagerank {
 
                 columns.get(i).remove(columnNumber);
             }
-
-            //show new matrix
-            // System.out.println("---new matrix---");
-            // for (int i = 0; i < rowsNumber; i++) {
-            //     for (int j = 0; j < colsNumber; j++) {
-
-            //         System.out.print(rows.get(i).get(j) + "\t");
-            //     }
-            //     System.out.println();
-            // }
-
-
         }
     }
 
-    public static String doubleToRationalString(double num1) {
-        double num2 = 1;
-        
-        // let this number without decimal
-        while (num1 % 1.0 != 0.0) {
-            num1 *= 10;
-            num2 *= 10;
-        }
-
-        // System.out.println("---after multiple--- num1: " + num1 + " num2: " + num2);
-
-        // find greatest common dividor(gcd)
-        int gcd = 1;
-        for (int i = 1; i <= num1 && i <= num2; i++) {
-            if (num1 % i == 0 && num2 % i == 0)
-                gcd = i;
-        }
-
-        // let two numbers divide by gcd
-        num1 /= gcd;
-        num2 /= gcd;
-
-        // System.out.println("---after divide by gcd--- num1: " + num1 + " num2: " + num2);
-
-        // get final string of rational number
-        int finalNum1 = (int) num1;
-        int finalNum2 = (int) num2;
-        String newString = finalNum2 == 1 ? String.valueOf(finalNum1)
-                : String.valueOf(finalNum1) + "/" + String.valueOf(finalNum2);
-        
-        return newString;
-    }
-
-    public static void iteration(List<List<String>> rows) {
-        if(rows.size() == 1) {
-            return;
-        }
-
-        
-        int times = 0;
-
-        String str = "1/" + rows.size();
-        String[] results = new String[rows.size()];
-        Arrays.fill(results, str);
-
-        double[] realResults = new double[rows.size()];
-        double size = rows.size();
-        double real = 1 / size;
-        Arrays.fill(realResults, real);
-
-        //show iteration
-        // System.out.println("------iteration 0------");
-        // for(int i = 0; i < results.length; i++) {
-        //     System.out.println(results[i]);
-        // }
-
-        // String[] secondResults = new String[rows.size()];
-
-        while(true) {
-            if(times >= 5){
-                realIteration(rows, realResults, times);
-                return;
-            }
-
-            times++;
-
-            String[] secondResults = new String[rows.size()];
-            double[] secondRealResults = new double[rows.size()];
-
-            // int[] changes = new int[results.length];
-
-            for(int i = 0; i < results.length; i++) {
-
-                // System.out.println("i = " + i);
-
-                //initial sum = 0;
-                BigInteger num1Up = new BigInteger("0");
-                BigInteger num1Down = new BigInteger("0");
-
-                // System.out.println("num1: " + num1Up + "/" + num1Down);
-
-                for(int j = 0; j < rows.get(i).size(); j++) {
-
-                    // System.out.println("j = " + j);
-
-                    if(rows.get(i).get(j).equals("0")) continue;
-
-                    //current node
-                    String[] strArray = rows.get(i).get(j).split("/");
-                    BigInteger num2Up = new BigInteger(strArray[0]);
-                    BigInteger num2Down = new BigInteger(strArray[1]);
-
-                    // System.out.println("node: " + num2Up + "/" + num2Down);
-
-                    //column
-                    String[] strArray2 = results[j].split("/");
-                    BigInteger resultUp = new BigInteger(strArray2[0]);
-                    BigInteger resultDown = new BigInteger(strArray2[1]);
-
-                    // System.out.println("column: " + resultUp + "/" + resultDown);
-
-                    //multiply node and column
-                    resultUp = resultUp.multiply(num2Up);
-                    resultDown = resultDown.multiply(num2Down);
-
-                    // System.out.println("after mul: " + resultUp + "/" + resultDown);
-
-                    //if previous sum = 0, then make this result as sum
-                    if(num1Up.equals(BigInteger.ZERO)) {
-                        // System.out.println("num1Up.equals(0)");
-                        num1Up = resultUp;
-                        num1Down = resultDown;
-
-                    // if previous sum != 0, then add this result to sum
-                    }else {
-
-                        BigInteger tempUp = num1Up.multiply(resultDown).add(num1Down.multiply(resultUp));
-                        BigInteger tempDown = num1Down.multiply(resultDown);
-
-                        num1Up = tempUp;
-                        num1Down = tempDown;
-                    }
-
-                    // System.out.println("after cal node: " + num1Up + "/" + num1Down);
-
-                }
-
-                //find gcd
-                BigInteger gcd = num1Up.gcd(num1Down);
-                // for (int k = 1; k <= num1Up && k <= num1Down; k++) {
-                //     if (num1Up % k == 0 && num1Down % k == 0)
-                //         gcd = k;
-                // }
-
-                //let two numbers divide by gcd
-                num1Up = num1Up.divide(gcd);
-                num1Down = num1Down.divide(gcd);
-
-                // System.out.println("gcd = " + gcd);
-
-                //change sum to string
-                secondResults[i] = num1Up.toString() + "/" + num1Down.toString();
-
-                // System.out.println("secondResults【" + i + "] = " + secondResults[i]);
-
-                //change sum to double
-                double num1UpReal = num1Up.doubleValue();
-                double num1DownReal = num1Down.doubleValue();
-                secondRealResults[i] = num1UpReal / num1DownReal;
-
-            }
-
-            int changeSum = 0;
-            for(int i = 0; i < rows.size(); i++) {
-                // System.out.println("previous real number: " + realResults[i]);
-                // System.out.println("real number: " + secondRealResults[i]);
-                // System.out.println("converge " + i + ": " + Math.abs(realResults[i] - secondRealResults[i]));
-
-                if(Math.abs(realResults[i] - secondRealResults[i]) < converge) {
-                    changeSum++;} 
-            }
-            if(changeSum == rows.size()) return;
-            // if(times > 5) break;
-
-            //clone the new results as the initial results
-            results = secondResults.clone();
-            realResults = secondRealResults.clone();
-
-            //show iteration
-            System.out.println("------iteration " + times + "------");
-            for(int i = 0; i < results.length; i++) {
-                System.out.println(results[i]);
-            }
-
-
-
-        }
-
-    }
-
-    public static void realIteration(List<List<String>> rows, double[] results, int times) {
-        while(true) {
-            times++;
-
-            double[] secondResults = new double[rows.size()];
-
-            for(int i = 0; i < rows.size(); i++) {
-
-                // double sum = 0;
-
-                for(int j = 0; j < rows.size(); j++) {
-                    if(rows.get(i).get(j).equals("0")) continue;
-
-                    double node = 0;
-                    if(rows.get(i).get(j).contains("/")) {
-
-                        //current node
-                        String[] strArray = rows.get(i).get(j).split("/");
-                        double nodeUp = Double.parseDouble(strArray[0]);
-                        double nodeDown = Double.parseDouble(strArray[1]);
-                        node = nodeUp / nodeDown;
-                    }else node = Double.parseDouble(rows.get(i).get(j));
-
-                    secondResults[i] += node * results[j];
-
-                }
-
-
-            }
-
-            int changeSum = 0;
-            for(int i = 0; i < rows.size(); i++) {
-                // System.out.println("previous real number: " + realResults[i]);
-                // System.out.println("real number: " + secondRealResults[i]);
-                // System.out.println("converge " + i + ": " + Math.abs(realResults[i] - secondRealResults[i]));
-
-                if(Math.abs(results[i] - secondResults[i]) < converge) changeSum++;
-            }
-            if(changeSum == rows.size()) return;
-
-            //clone the new results as the initial results
-            results = secondResults.clone();
-
-            //show iteration
-            System.out.println("------iteration " + times + "------");
-            for(int i = 0; i < results.length; i++) {
-                System.out.println(results[i]);
-            }
-        }
-    }
-
-    public static void readText(List<List<Double>> columns, List<Integer> countColumns, Map<String, Integer> nameMap) {
+    public static void readText(List<List<Double>> columns, List<Integer> countColumns, List<String> nameList) {
         BufferedReader reader;
 
-        // List<Integer> countColumn = new LinkedList<>();
+        Map<String, Integer> nameMap = new HashMap<>();
 
 		try {
 			reader = new BufferedReader(new FileReader("routes.txt"));
@@ -312,10 +141,10 @@ class Pagerank {
                 String departure = strArray[2];
                 String destination = strArray[4];
 
-                System.out.println(count + "-" + departure + "-" + destination);
+                // System.out.println(count + "-" + departure + "-" + destination);
 
                 if(!nameMap.containsKey(departure)){
-                    List<Double> newColumn = new LinkedList<>();
+                    List<Double> newColumn = new ArrayList<>();
 
                     int i = 0;
 
@@ -328,12 +157,13 @@ class Pagerank {
                     columns.add(newColumn);
 
                     nameMap.put(departure, i);
+                    nameList.add(departure);
 
                     countColumns.add(0);
                 }
 
                 if(!nameMap.containsKey(destination)){
-                    List<Double> newColumn = new LinkedList<>();
+                    List<Double> newColumn = new ArrayList<>();
 
                     int i = 0;
 
@@ -346,6 +176,7 @@ class Pagerank {
                     columns.add(newColumn);
 
                     nameMap.put(destination, i);
+                    nameList.add(destination);
 
                     countColumns.add(0);
                 }
@@ -382,14 +213,40 @@ class Pagerank {
     }
 
     public static void main(String args[]) {
-        List<List<Double>> columns = new LinkedList<>();
+        List<List<Double>> columns = new ArrayList<>();
         List<Integer> countColumns = new LinkedList<>();
-        Map<String, Integer> nameMap = new HashMap<>();
+        List<String> nameList = new LinkedList<>();
  
-        readText(columns, countColumns, nameMap);
+        System.out.println("start readText");
 
-        System.out.println("columns size: " + columns.size());
-        System.out.println("countColumns: " + countColumns);
+        readText(columns, countColumns, nameList);
+
+        // System.out.println("columns size: " + columns.size());
+        // System.out.println("countColumns: " + countColumns);
+
+        System.out.println("start removeNode");
+
+        removeNode(columns, countColumns, nameList); 
+
+        // System.out.println("columns size: " + columns.size());
+        // System.out.println("countColumns: " + countColumns);
+
+        System.out.println("start convertMatrix");
+
+        convertMatrix(columns, countColumns);
+
+        // System.out.println("columns size: " + columns.size());
+        // System.out.println("nameList size: " + nameList.size());
+
+        System.out.println("start pagerank");
+
+        double[] vector = pagerank(columns);
+
+        System.out.println("start rank");
+
+        Map<Double, String> treeMap = rank(vector, nameList);
+
+        show(treeMap);
 
         // for(int i = 0; i < columns.size(); i++) {
         //     for(int j = 0; j < columns.size(); j++) {
@@ -398,8 +255,5 @@ class Pagerank {
         //     System.out.println();
         // }
 
-        removeNode(columns, countColumns); 
-
-        // iteration(rows);
     }
 }
