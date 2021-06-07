12615e9ca36c7e36c1cf15716d262aa4f9a5f258
yukunzhang115 <77864795+yukunzhang115@users.noreply.github.com>
2021-06-06 19:12:05 -0700

    Version 2
    1. changed the arrayList to array
    2. Mixed the bug in function readText() about mistakenly adding more count in countColumns.

--- Pagerank.java
+++ Pagerank.java
@@ -34,9 +34,73 @@ class Pagerank {
         return treeMap;
     }
 
+    public static double[] pagerank(double[][] matrix) {
+        double[] vector = new double[matrix.length];
+        Arrays.fill(vector, 1.0 / matrix.length);
+
+        // System.out.println("vector.length: " + vector.length);
+        // for(int i = 0; i < vector.length; i++) {
+        //     System.out.print(vector[i] + " ");
+        // }
+        // System.out.println();
+
+        double[] newVector;
+
+        int times = 0;
+
+        while(true) {
+            times++;
+
+            System.out.println("----------" + times + "-----------");
+
+            newVector = new double[matrix.length];
+
+            // for(int i = 0; i < newVector.length; i++) {
+            //     System.out.print(newVector[i] + " ");
+            // }
+            // System.out.println();
+            // System.out.println();
+
+            int changeCount = 0;
+
+            for(int i = 0; i < matrix.length; i++) {
+                for(int j = 0; j < matrix.length; j++) {
+                    newVector[i] += matrix[i][j] * vector[j];
+
+                    // System.out.print(newVector[i] + " ");
+                }
+
+                if(Math.abs(newVector[i] - vector[i]) <= converge) changeCount++;
+
+                // System.out.println();
+                // System.out.println();
+            }
+
+            System.out.println(changeCount);
+
+            // for(int i = 0; i < newVector.length; i++) {
+            //     System.out.print(newVector[i] + " ");
+            // }
+            // System.out.println();
+
+            if(changeCount >= vector.length) return newVector;
+
+            // vector = newVector.clone();
+            System.arraycopy(newVector, 0, vector, 0, newVector.length);
+
+            // System.out.println();
+            // System.out.println();
+
+        }
+
+
+    }
+
     public static double[] pagerank(List<List<Double>> columns) {
         double[] vector = new double[columns.size()];
         Arrays.fill(vector, 1.0 / columns.size());
+
+        System.out.println("vector.length: " + vector.length);
 
         double[] newVector;
 
@@ -74,14 +138,29 @@ class Pagerank {
 
     }
 
-    public static void convertMatrix(List<List<Double>> columns, List<Integer> countColumns) {
+    public static double[][] convertMatrix(List<List<Double>> columns, List<Integer> countColumns) {
+        double[][] matrix = new double[columns.size()][columns.size()];
+
         for(int i = 0; i < columns.size(); i++) {
-            // System.out.println(i);
+            
             double num = 1.0 / countColumns.get(i);
+
+            // System.out.println(i);
+            // System.out.println();
+            // System.out.println("-----------num: " + num);
+            // System.out.println();
+
             for(int j = 0; j < columns.size(); j++) {
-                if(columns.get(i).get(j) == 1.0) columns.get(i).set(j, num);
+                if(columns.get(i).get(j) == 1234567890.0) matrix[j][i] = num;
             }
+
+            // matrix[i] = columns.get(i).stream().mapToDouble(Double::doubleValue).toArray();
+
+            // System.out.println(columns.get(i));
         }
+
+        return matrix;
+
     }
 
     public static void removeNode(List<List<Double>> columns, List<Integer> countColumns, List<String> nameList) {
@@ -90,8 +169,6 @@ class Pagerank {
         while (true) {
             count++;
 
-            boolean isEnd = false;
-
             // find a column with count of 1 or 0
             int columnNumber = -1;
             for(int i = 0; i < countColumns.size(); i++) {
@@ -99,10 +176,8 @@ class Pagerank {
                     columnNumber = i;
                     break;
                 }
-                
-                if(i == countColumns.size() - 1) isEnd = true;
             }
-            if(isEnd) break;
+            if(columnNumber == -1) break;
 
             // System.out.println(count + "-columnNumber: " + columnNumber);
 
@@ -113,7 +188,7 @@ class Pagerank {
 
             // modify count of other columns AND delete that row
             for(int i = 0; i < columns.size(); i++) {
-                if(columns.get(i).get(columnNumber) == 1.0) countColumns.set(i, countColumns.get(i) - 1);
+                if(columns.get(i).get(columnNumber) == 1234567890.0) countColumns.set(i, countColumns.get(i) - 1);
 
                 columns.get(i).remove(columnNumber);
             }
@@ -183,15 +258,18 @@ class Pagerank {
 
                 int indexOfDeparture = nameMap.get(departure);
                 int indexOfDestination = nameMap.get(destination);
-                columns.get(indexOfDeparture).set(indexOfDestination, 1.0);
-                countColumns.set(indexOfDeparture, countColumns.get(indexOfDeparture) + 1);
 
+                if(columns.get(indexOfDeparture).get(indexOfDestination) == 0.0) {
+                    columns.get(indexOfDeparture).set(indexOfDestination, 1234567890.0);
+                    countColumns.set(indexOfDeparture, countColumns.get(indexOfDeparture) + 1);
+                }
+
+                // columns.get(indexOfDeparture).set(indexOfDestination, 1.0);
+                
+
+                // System.out.println("countColumns: " + countColumns);
                 // for(int i = 0; i < columns.size(); i++) {
-                //     for(int j = 0; j < columns.size(); j++) {
-                //         System.out.print(columns.get(j).get(i) + " ");
-                //     }
-                //     System.out.println(countColumn);
-                //     System.out.println();
+                //     System.out.println(columns.get(i));
                 // }
 
 				// read next line
@@ -214,7 +292,7 @@ class Pagerank {
 
     public static void main(String args[]) {
         List<List<Double>> columns = new ArrayList<>();
-        List<Integer> countColumns = new LinkedList<>();
+        List<Integer> countColumns = new ArrayList<>();
         List<String> nameList = new LinkedList<>();
  
         System.out.println("start readText");
@@ -222,7 +300,14 @@ class Pagerank {
         readText(columns, countColumns, nameList);
 
         // System.out.println("columns size: " + columns.size());
+        
         // System.out.println("countColumns: " + countColumns);
+        
+        // for(int i = 0; i < columns.size(); i++) {
+        //     System.out.println("--------------" + i);
+        //     System.out.println(columns.get(i));
+        //     System.out.println();
+        // }
 
         System.out.println("start removeNode");
 
@@ -231,18 +316,34 @@ class Pagerank {
         // System.out.println("columns size: " + columns.size());
         // System.out.println("countColumns: " + countColumns);
 
+        // for(int i = 0; i < columns.size(); i++) {
+        //     System.out.println("--------------" + i);
+        //     System.out.println("--------------" + countColumns.get(i));
+        //     System.out.println(columns.get(i));
+        //     System.out.println();
+        // }
+
         System.out.println("start convertMatrix");
 
-        convertMatrix(columns, countColumns);
+        double[][] matrix = convertMatrix(columns, countColumns);
+
+        // for(int i = 0; i < matrix.length; i++) {
+        //     System.out.println("--------------" + i);
+        //     System.out.println("--------------" + countColumns.get(i));
+        //     for(int j = 0; j < matrix.length; j++) {
+        //         System.out.print(matrix[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
 
         // System.out.println("columns size: " + columns.size());
         // System.out.println("nameList size: " + nameList.size());
 
         System.out.println("start pagerank");
 
-        double[] vector = pagerank(columns);
+        double[] vector = pagerank(matrix);
 
-        System.out.println("start rank");
+        // System.out.println("start rank");
 
         Map<Double, String> treeMap = rank(vector, nameList);
 
